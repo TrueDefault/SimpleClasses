7f905b72c238bbc9fdaa8b4f5db18332bcd32892
Eugene Chau <eugene.chau01@gmail.com>
2017-06-22 22:54:18 -0700

    Convert indentation spaces into tabs

--- src/echau/programming/simpleclasses/Automobile.java
+++ src/echau/programming/simpleclasses/Automobile.java
@@ -7,62 +7,65 @@ package echau.programming.simpleclasses;
  * @version June 15, 2017
  */
 public class Automobile {
-    private double mpg;
-    private double gallons;
-    private double mileage;
-    
-    /**
-     * Creates a new Automobile with the set miles per gallon. The gallons of fuel
-     * and the mileage are both set to 0.
-     */
-    public Automobile(final double mpg) {
-        this.mpg = mpg;
-        gallons = 0;
-    }
-    
-    /**
-     * Fills up this Automobile's gas tank with the specified number of gallons.
-     * 
-     * @param gallons The number of gallons to fill up.
-     */
-    public void fillUp(final double gallons) {
-        this.gallons += gallons;
-    }
-    
-    /**
-     * Removes the correct amount of gas in the tank after this Automobile travels 
-     * the specified number of miles. Increments the mileage by the number of miles 
-     * travelled. If the Automobile runs out of gas midway, the amount of gallons 
-     * of fuel in the tank is set to 0. The new mileage is still calculated according
-     * to how many miles were travelled.
-     * 
-     * @param miles The number of miles to travel.
-     */
-    public void takeTrip(final double miles) {
-        if (this.gallons * this.mpg < miles) {
-            this.mileage += this.gallons * this.mpg;
-            this.gallons = 0;
-        } else {
-            this.mileage += miles;
-            this.gallons -= (miles / this.mpg);
-        }
-    }
-    
-    /**
-     * Returns the amount of fuel in gallons left in the tank.
-     * 
-     * @return How much fuel in gallons there is remaining in this Automobile's tank.
-     */
-    public double reportFuel() {
-        return this.gallons;
-    }
-    
-    /**
-     * Gets the amount of miles this Automobile has travelled, and returns it.
-     * 
-     * @return The mileage of this Automobile.
-     */
-    public double getMileage() {
-        return this.mileage;
-    }
+	private double mpg;
+	private double gallons;
+	private double mileage;
+
+	/**
+	 * Creates a new Automobile with the set miles per gallon. The gallons of
+	 * fuel and the mileage are both set to 0.
+	 */
+	public Automobile(final double mpg) {
+		this.mpg = mpg;
+		gallons = 0;
+	}
+
+	/**
+	 * Fills up this Automobile's gas tank with the specified number of gallons.
+	 * 
+	 * @param gallons
+	 *            The number of gallons to fill up.
+	 */
+	public void fillUp(final double gallons) {
+		this.gallons += gallons;
+	}
+
+	/**
+	 * Removes the correct amount of gas in the tank after this Automobile
+	 * travels the specified number of miles. Increments the mileage by the
+	 * number of miles travelled. If the Automobile runs out of gas midway, the
+	 * amount of gallons of fuel in the tank is set to 0. The new mileage is
+	 * still calculated according to how many miles were travelled.
+	 * 
+	 * @param miles
+	 *            The number of miles to travel.
+	 */
+	public void takeTrip(final double miles) {
+		if (this.gallons * this.mpg < miles) {
+			this.mileage += this.gallons * this.mpg;
+			this.gallons = 0;
+		} else {
+			this.mileage += miles;
+			this.gallons -= (miles / this.mpg);
+		}
+	}
+
+	/**
+	 * Returns the amount of fuel in gallons left in the tank.
+	 * 
+	 * @return How much fuel in gallons there is remaining in this Automobile's
+	 *         tank.
+	 */
+	public double reportFuel() {
+		return this.gallons;
+	}
+
+	/**
+	 * Gets the amount of miles this Automobile has travelled, and returns it.
+	 * 
+	 * @return The mileage of this Automobile.
+	 */
+	public double getMileage() {
+		return this.mileage;
+	}
 }
--- src/echau/programming/simpleclasses/Circle.java
+++ src/echau/programming/simpleclasses/Circle.java
@@ -7,59 +7,63 @@ package echau.programming.simpleclasses;
  * @version June 15, 2017
  */
 public class Circle {
-    private double radius;
-    
-    /**
-     * Constructs a circle with a default radius of 0.
-     */
-    public Circle() {
-        this.radius = 0;
-    }
-    
-    /**
-     * Constructs a circle with the specified radius.
-     * 
-     * @throws IllegalArgumentException If the radius is less than 0.
-     */
-    public Circle(final double radius) {
-        if (radius < 0) {
-            throw new IllegalArgumentException("Negative radius: " + radius);
-        }
-        this.radius = radius;
-    }
-    
-    /**
-     * Calculates this circle's area with the formula <code>Area = \u03C0r^2</code>.
-     * 
-     * @return This circle's area.
-     */
-    public double area() {
-        return Math.PI * Math.pow(this.radius, 2);
-    }
-    
-    /**
-     * Calculates this circle's diameter with the formula <code>d = 2r</code>.
-     * 
-     * @return This circle's diameter.
-     */
-    public double diameter() {
-        return this.radius * 2;
-    }
-    
-    public double getRadius() {
-        return this.radius;
-    }
-    
-    /**
-     * Sets this circle's radius.
-     * 
-     * @param radius The new radius.
-     * @throws IllegalArgumentException If the radius is less than 0.
-     */
-    public void setRadius(final double radius) {
-        if (radius < 0) {
-            throw new IllegalArgumentException("Negative radius: " + radius);
-        }
-        this.radius = radius;
-    }
+	private double radius;
+
+	/**
+	 * Constructs a circle with a default radius of 0.
+	 */
+	public Circle() {
+		this.radius = 0;
+	}
+
+	/**
+	 * Constructs a circle with the specified radius.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If the radius is less than 0.
+	 */
+	public Circle(final double radius) {
+		if (radius < 0) {
+			throw new IllegalArgumentException("Negative radius: " + radius);
+		}
+		this.radius = radius;
+	}
+
+	/**
+	 * Calculates this circle's area with the formula
+	 * <code>Area = \u03C0r^2</code>.
+	 * 
+	 * @return This circle's area.
+	 */
+	public double area() {
+		return Math.PI * Math.pow(this.radius, 2);
+	}
+
+	/**
+	 * Calculates this circle's diameter with the formula <code>d = 2r</code>.
+	 * 
+	 * @return This circle's diameter.
+	 */
+	public double diameter() {
+		return this.radius * 2;
+	}
+
+	public double getRadius() {
+		return this.radius;
+	}
+
+	/**
+	 * Sets this circle's radius.
+	 * 
+	 * @param radius
+	 *            The new radius.
+	 * @throws IllegalArgumentException
+	 *             If the radius is less than 0.
+	 */
+	public void setRadius(final double radius) {
+		if (radius < 0) {
+			throw new IllegalArgumentException("Negative radius: " + radius);
+		}
+		this.radius = radius;
+	}
 }
